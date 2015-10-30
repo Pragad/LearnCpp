b02c4b7f2fec62b45ebccec9c3254164d72b992d
talk2pragadheesh@gmail.com <pragadhe@PRAGADHE-PC.hq.netapp.com>
2015-10-29 17:27:07 -0700

    Passing Array using Reference to Pointer

--- changingClassPrivateVar.cpp
+++ changingClassPrivateVar.cpp
@@ -11,7 +11,18 @@ class A
 public:
     void increment()
     {
-        (*_x)++;
+        cout << endl;
+        cout << "Inc1 _x         : " << _x << endl;
+        cout << "Inc1 *_arr      : " << *_arr << endl;
+
+        (*_x) = 3;
+        (*_arr)[0] = 1;
+        (*_arr)[1] = 2;
+        (*_arr)[2] = 3;
+
+        cout << endl;
+        cout << "Inc2 _x         : " << _x << endl;
+        cout << "Inc2 *_arr      : " << *_arr << endl;
     }
 
     int getX()
@@ -22,40 +33,54 @@ public:
     // Get the address of the Variable that is passed in main.
     // x will now have &y2
     // x addr: 0x246ff08
-    A (int* &x)
+    A (int* &arr, int* &x)
     {
-        cout << "x : " << x << endl;
+        cout << endl;
+        cout << "x      : " << x << endl;
+        cout << "arr    : " << arr << endl;
+
         // _x will now have &y2
         // So *_x can be used to get to the value of y2
         _x = x;
-        cout << "_x : " << _x << endl;
+        *_arr = arr;
+        cout << "_x     : " << _x << endl;
+        cout << "*_arr  : " << *_arr << endl;
     }
 
 private:
     // _x will have &y2
     int* _x;
+    int** _arr;
 };
 
 int main()
 {
-    cout << "Changing Class Private Variable" << endl;
+    cout << "Changing Class Private Variable" << endl << endl;
 
     // y2 Addr: 0x246ff08
     // y1 Addr: 0x246ff04
-    int y2 = 5;
+    int y2;
+    int arr[5];
+
     int *y1 = &y2;
+    int *yArr = arr;
 
-    cout << "y1 Addr: " << &y1 << endl;
-    cout << "y2 Addr: " << &y2 << endl;
 
-    A *obj1 = new A(y1);
+    cout << "y1 value   : " << y1 << endl;
+    cout << "y2 Addr    : " << &y2 << endl;
+
+    cout << "yArr value : " << yArr << endl;
+    cout << "arr Addr   : " << &arr << endl;
+
+    A *obj1 = new A(yArr, y1);
     // This gives a compile time error. warning: initialization of non-const reference int *&' from rvalue `int *'
     // A *obj1 = new A(&y2);
     obj1->increment();
 
+    cout << endl;
     cout << "obj1.getX(): " << obj1->getX() << endl;
-    cout << "y1: " << y1 << endl;
-    cout << "y2: " << y2 << endl;
+    cout << "y2         : " << y2 << endl;
+    cout << "[0]: " << arr[0] << "; [1]: " << arr[1] << "; [2]: " << arr[2] << endl;
 
     cout << endl;
     return 0;
