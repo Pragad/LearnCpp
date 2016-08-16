0dd79e82a0f015510ca8a1bc9e33644d816a313e
SyncMr <talk2pragadheesh@gmail.com>
2016-08-15 17:57:09 -0700

    Adding PassByRef

---
+++ PassByReference.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include <vector>
+#include <unordered_map>
+#include <stack>
+#include <string>
+using namespace std;
+
+void startGame(const string& str,
+               vector< stack<uint32_t> >& robotWorld,
+               unordered_map<uint32_t, uint32_t>& blockIndex)
+{
+}
+
+void moveBlock(vector< stack<uint32_t> >& robotWorld,
+               unordered_map<uint32_t, uint32_t>& blockIndex,
+               uint32_t a,
+               uint32_t b,
+               uint32_t aIdx,
+               uint32_t bIdx)
+{
+}
+int main()
+{
+
+    startGame(line,  robotWorld, blockIndex);
+}
