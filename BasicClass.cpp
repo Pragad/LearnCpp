447573d3f17e50bb38695a29f2e53dc8d96d84a6
SyncMr <talk2pragadheesh@gmail.com>
2016-09-02 00:29:43 -0700

    Basic Class and tips

--- BasicClass.cpp
+++ BasicClass.cpp
@@ -10,16 +10,18 @@ class B
 
 class A
 {
-    private:
-        B b3;           // A's size is now 4
-        B *b1;          // A's size is now 8
-        //B &b2; uninitialized reference member in 'class A'
+    public:
+        B *b1;          // 0x61fefc A's size is now 4. As this is a pointer, sizeof(b4) is 1
+        B b3;           // 0x61ff00 A's size is now 8. Since 'class B' is empty sizeof(b3)= 1
+        //B &b2;        // ERR: uninitialized reference member in 'class A'
 
     public:
         // Default Values:
-        bool isValid;   // A's size is now 12
-        int i;          // A's size is now 16
-        vector<int> v;  // A's size is now 28
+        bool isValid;   // 0x61ff01 A's size is now 8
+        int i;          // 0x61ff04 A's size is now 12
+        vector<int> v;  // 0x61ff08 A's size is now 24
+        B b4;           // 0x61ff14 A's size is now 28. But since 'class B' is empty sizeof(b4) is 1
+        int j;          // A's size is 32
 };
 
 class C
@@ -35,25 +37,40 @@ class C
 
 int main()
 {
-    A a;
-    B b1;
-    B b2(b1);
-    B b3 = b1;
+    {
+        A a;
+        B b1;
+        B b2(b1);
+        B b3 = b1;
 
-    cout << "A's a size: " << sizeof(a) << endl;
-    cout << "B's b1 size: " << sizeof(b1) << endl;
-    cout << "B's b2 size: " << sizeof(b2) << endl;
-    cout << "B's b3 size: " << sizeof(b3) << endl;
+        // IMP:
+        // &a and &a.b1 ARE SAME
+        cout << "A's a size: " << sizeof(a) << "; at : " << &a << endl;
+        cout << "A's a.b1 size: " << sizeof(a.b1) <<  "; at : " << &(a.b1) << endl;
+        cout << "A's a.b3 size: " << sizeof(a.b3) <<  "; at : " << &(a.b3) << endl << endl;
+        cout << "A's a.isValid: " << sizeof(a.isValid) <<  "; at : " << &(a.isValid) << endl;
+        cout << "A's a.i size: " << sizeof(a.i) <<  "; at : " << &(a.i) << endl;
+        cout << "A's a.v size: " << sizeof(a.v) <<  "; at : " << &(a.v) << endl;
+        cout << "A's a.b4 size: " << sizeof(a.b4) <<  "; at : " << &(a.b4) << endl;
+        cout << "A's a.j size: " << sizeof(a.j) <<  "; at : " << &(a.j) << endl << endl;
 
-    // Default Values
-    cout << "A's isValid: " << a.isValid << endl;
-    cout << "A's i: " << a.i << endl;
-    cout << "A's v: " << a.v.size() << endl;
+        cout << "B's b1 size: " << sizeof(b1) <<  "; at : " << &(b1) << endl;
+        cout << "B's b2 size: " << sizeof(b2) <<  "; at : " << &(b2) << endl;
+        cout << "B's b3 size: " << sizeof(b3) <<  "; at : " << &(b3) << endl << endl;
 
-    // Want Compile Time failures
-    C c2;
-    //C c1(c2);     // error: 'C::C(const C&)' is private 
-    //C c3 = c2;    // error: within this context
+        // Default Values
+        cout << "VALUES" << endl;
+        cout << "A's isValid: " << a.isValid << endl;
+        cout << "A's i: " << a.i << endl;
+        cout << "A's v: " << a.v.size() << endl;
+    }
+
+    {
+        // Want Compile Time failures
+        C c2;
+        //C c1(c2);     // error: 'C::C(const C&)' is private 
+        //C c3 = c2;    // error: within this context
+    }
 
     return 0;
 }
