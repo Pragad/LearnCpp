f21f59c9104858225c5e7703be17cb02ca0521fc
SyncMr <talk2pragadheesh@gmail.com>
2016-10-06 11:35:12 -0700

    Threads

--- ThreadsMutex.cpp
+++ ThreadsMutex.cpp
@@ -3,6 +3,9 @@
 #include <mutex>
 using namespace std;
 
+// -----------------------------------------------------------------------------------------
+// PROBLEM 1. Increment a value using mutex
+// -----------------------------------------------------------------------------------------
 // A Global variable x that will be shared by the threads
 uint32_t x = 0;
 
@@ -17,6 +20,8 @@ void incrementX()
 mutex mtx2;
 void incrementXMtx()
 {
+    // With unique_lock, we don't have to manually unlock. When the function goes out of scope
+    // automatic unlock of the mutex happens
     unique_lock<mutex> lock(mtx2);
     for (uint32_t i = 0; i < 100; i++)
     {
@@ -24,10 +29,14 @@ void incrementXMtx()
     }
 }
 
+// -----------------------------------------------------------------------------------------
+// PROBLEM 2. Print Chars using Mutex
+//
 // Race Condition Here.
 // We can't predict the order of execution.
 // The order of execution depends on thread scheduling algorithm
 // Multiple Runs of the same program will give different output
+// -----------------------------------------------------------------------------------------
 void printChars(uint32_t num, char ch)
 {
     for (uint32_t i = 0; i < num; i++)
@@ -50,27 +59,12 @@ void printCharsMutex(uint32_t num, char ch)
     cout << endl;
 }
 
+// -----------------------------------------------------------------------------------------
+// Main Function
+// -----------------------------------------------------------------------------------------
 int main()
 {
     // Example 1
-    {
-        cout << "Without Mutex" << endl;
-        thread th1 (printChars, 10, '*');
-        thread th2 (printChars, 10, '$');
-
-        th1.join();
-        th2.join();
-
-        cout << "With Mutex" << endl;
-        thread th3 (printCharsMutex, 10, '*');
-        thread th4 (printCharsMutex, 10, '$');
-
-        // Main will wait for the threads to finish execution before continuing its work
-        th3.join();
-        th4.join();
-    }
-
-    // Example 2
     {
         x = 0;
         thread th5 (incrementX);
@@ -87,6 +81,24 @@ int main()
         th7.join();
         th8.join();
         cout << "With Mutex X Val: " << x << endl;
+    }
+
+    // Example 2
+    {
+        cout << "Without Mutex" << endl;
+        thread th1 (printChars, 10, '*');
+        thread th2 (printChars, 10, '$');
+
+        th1.join();
+        th2.join();
+
+        cout << "With Mutex" << endl;
+        thread th3 (printCharsMutex, 10, '*');
+        thread th4 (printCharsMutex, 10, '$');
+
+        // Main will wait for the threads to finish execution before continuing its work
+        th3.join();
+        th4.join();
     }
 
     cout << endl;
