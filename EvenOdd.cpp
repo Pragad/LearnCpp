ebd044835a333c224cdd468ca7bd5103d61471a6
Pragad <talk2pragadheesh@gmail.com>
2017-02-12 18:31:07 -0800

    Even Odd

--- EvenOdd.cpp
+++ EvenOdd.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 using namespace std;
 
+#define DEBUG 1
+
 int main()
 {
     int a = 4;
 
-    if (!(a & 1))
+#ifdef DEBUG
+    cout << "Even Odd" << endl;
+#endif
+
+    //if (!(a & 1))
+    if (a % 2 == 0)
     {
         cout << "Num is even" << endl;
     }
